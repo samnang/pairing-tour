e55e12e5ec3862fcd441045bcbe101e557fec6ef
Mann <chanmann@instedd.org>
2010-10-18 20:10:29 +0700

    add sort letter and ignore vi temporary file

--- natural-sort/lib/natural_sort.rb
+++ natural-sort/lib/natural_sort.rb
@@ -1,9 +1,22 @@
 module NaturalSort
 	def natural_sort
-		self.sort {|x, y| x.to_i <=> y.to_i }
+		self.sort { |x, y| 
+      if x.is_numeric? && y.is_numeric?
+        x.to_i <=> y.to_i
+      else
+        x <=> y
+      end
+    }
 	end
+
 end
 
 class Array
 	include NaturalSort	
+end
+
+class String
+  def is_numeric?
+    self.to_i.to_s == self
+  end
 end
--- natural-sort/spec/natural_sort_spec.rb
+++ natural-sort/spec/natural_sort_spec.rb
@@ -5,13 +5,22 @@ require 'lib/natural_sort'
 describe NaturalSort do	
 	it "should order correctly in numerics" do
 		array = ["10", "2"]
-		
+
 		result = array.natural_sort 
-		
+
 		result.should == ["2", "10"]
 	end
+
+  it "should order letter correctly" do
+    array = %w{banana apple grape orange mango}
+
+    result = array.natural_sort
+
+    result.should == ["apple", "banana", "grape", "mango", "orange"]
+  end
 	
-	it "should order letter followed by numeric"
+	it "should order letter followed by numeric" 
+    
 end
 
 
