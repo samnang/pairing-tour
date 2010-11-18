59030cd9d132f0580f256bf8467803bb2f016aca
Mann <chanmann@instedd.org>
2010-11-18 19:49:05 +0700

    implement natural sort

--- natural-sort/lib/natural_sort.rb
+++ natural-sort/lib/natural_sort.rb
@@ -1,22 +1,30 @@
 module NaturalSort
 	def natural_sort
-		self.sort { |x, y| 
-      if x.is_numeric? && y.is_numeric?
-        x.to_i <=> y.to_i
-      else
-        x <=> y
-      end
+		self.sort { |x, y|	
+			convert_to_array_of_letters_and_numbers(x) <=> 
+			convert_to_array_of_letters_and_numbers(y)
     }
 	end
-
+	
+	def convert_to_array_of_letters_and_numbers(str)
+		array = split_letters_and_numbers(str)
+		
+		convert_numeric_elements(array)
+	end
+	
+	def convert_numeric_elements(array)
+		1.step((array.size - 1), 2) do |index|
+			array[index] = array[index].to_i
+		end
+		
+		array
+	end
+	
+	def split_letters_and_numbers(str)
+		str.split(/(\d+)/)		
+	end
 end
 
 class Array
 	include NaturalSort	
-end
-
-class String
-  def is_numeric?
-    self.to_i.to_s == self
-  end
 end
--- natural-sort/spec/natural_sort_spec.rb
+++ natural-sort/spec/natural_sort_spec.rb
@@ -18,9 +18,32 @@ describe NaturalSort do
 
     result.should == ["apple", "banana", "grape", "mango", "orange"]
   end
-	
-	it "should order letter followed by numeric" 
+  
+  Test_Data = {
+								["a10", "a2"] => ["a2", "a10"],
+								["a2a", "a1b"] => ["a1b", "a2a"],
+								["alpha 2", "alpha 10", "alpha 1"] => ["alpha 1", "alpha 2", "alpha 10"]
+							}
+  
+  Test_Data.each do |input, expected_result|
+  	it "should order [#{input.join(', ')}]" do
+  		result = input.natural_sort
+  		
+  		result.should == expected_result
+  	end
+  end
     
 end
+
+describe "Spikes" do
+	it "should compare correctly between to arrays" do
+		array1 = ['a', 2, 'b']
+		array2 = ['a', 10]
+		
+		(array1 <=> array2).should == -1
+	end
+end
+
+
 
 
